cd5847c34545ab57a587bb1ca1070e32317e9938
Ibrahim Maâzou Djahadi <ibrahimdjahadi46@gmail.com>
2026-06-03 07:08:23 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:assignment1/models/index.dart';
+// import 'package:assignment1/screens/home.dart';
+// import 'package:flutter/material.dart';
+
+void main() {
+  // runApp(const MyApp());
+  initializeStores();
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(home: HomeScreen());
+//   }
+// }
