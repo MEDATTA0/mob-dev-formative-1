28767abcff74b30094fe3a2c31185b458f93ba10
Ibrahim Maâzou Djahadi <ibrahimdjahadi46@gmail.com>
2026-06-03 10:40:48 -0230

    feat: add empty routes and README files

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:assignment1/models/index.dart';
-// import 'package:assignment1/screens/home.dart';
-// import 'package:flutter/material.dart';
+import 'package:assignment1/screens/home.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(const MyApp());
+  runApp(const MyApp());
   initializeStores();
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(home: HomeScreen());
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+      routes: {
+        // Add a routes here
+      },
+    );
+  }
+}
